7b9468bacb8555094e3de304c64cce17643f994a
Sergey <livegrafics@rambler.ru>
2022-08-19 13:35:48 +0300

    Сделано заполнение левого рабочего списка и добавлены несколько дополнительных элементов оконного интерфейса

--- src/main/java/ru/gb/netfilewarehouse/NetFileWarehouseController.java
+++ src/main/java/ru/gb/netfilewarehouse/NetFileWarehouseController.java
@@ -1,5 +1,7 @@
 package ru.gb.netfilewarehouse;
 
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -13,19 +15,78 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.*;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class NetFileWarehouseController implements Initializable {
     public static Stage mainStage;
+    @FXML
+    public TableView<FileInfo> filesTableLocal;
+    public ComboBox disksBox;
+    public TextField pathField;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("in Init");
         showAuthDialog(mainStage);
 
+        TableColumn<FileInfo,String> fileTypeColumn = new TableColumn<>();
+        fileTypeColumn.setCellValueFactory(param->new SimpleStringProperty(param.getValue().getType().getName()));
+        fileTypeColumn.setPrefWidth(20);
+
+        TableColumn<FileInfo,String> filenameColumn = new TableColumn<>("Имя");
+        filenameColumn.setCellValueFactory(param->new SimpleStringProperty(param.getValue().getFilename()));
+        filenameColumn.setPrefWidth(267);
+
+        TableColumn<FileInfo,Long> fileSizeColumn=new TableColumn<>("Размер");
+        fileSizeColumn.setCellValueFactory(param->new SimpleObjectProperty<>(param.getValue().getSize()));
+        fileSizeColumn.setPrefWidth(141);
+        fileSizeColumn.setCellFactory(column->{
+            return new TableCell<FileInfo,Long>(){
+                @Override
+                protected void updateItem(Long item,boolean empty){
+                    if (item==null||empty){
+                        setText(null);
+                        setStyle("");
+                    } else {
+                        String text=String.format("%,d bytes",item);
+                        if (item==-1L){
+                            text="[DIR]";
+                        }
+                        setText(text);
+                    }
+                }
+            };
+        });
+
+        filesTableLocal.getColumns().addAll(fileTypeColumn,filenameColumn,fileSizeColumn);
+        filesTableLocal.getSortOrder().add(fileTypeColumn);
+
+        disksBox.getItems().clear();
+        for (Path p: FileSystems.getDefault().getRootDirectories()){
+            disksBox.getItems().add(p.toString());
+        }
+        disksBox.getSelectionModel().select(0);
+
+        updateList(Paths.get("."));
+
     }
 
+    public void updateList (Path path){
+        try {
+            pathField.setText(path.normalize().toAbsolutePath().toString());
+            filesTableLocal.getItems().clear();
+            filesTableLocal.getItems().addAll(Files.list(path).map(FileInfo::new).collect(Collectors.toList()));
+            filesTableLocal.sort();
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING,"Не удалось обновить список файлов",ButtonType.OK);
+            alert.showAndWait();
+        }
+
+    }
     public void showAuthDialog(Stage stage) {
         Stage dialogAuth = new Stage();
         dialogAuth.setResizable(false);
@@ -67,4 +128,10 @@ public class NetFileWarehouseController implements Initializable {
         dialogAuth.showAndWait();
     }
 
+    public void btnPathUpAction(ActionEvent actionEvent) {
+       Path upperPath =Paths.get(pathField.getText()).getParent();
+       if (upperPath!=null){
+           updateList(upperPath);
+       }
+    }
 }
